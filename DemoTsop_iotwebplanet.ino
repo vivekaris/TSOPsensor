2e2563da4defa4cf3ffe78952c3f8305d7a70080
Iotwebplanet <iotwebplanet.bvm@gmail.com>
2017-08-20 16:04:06 +0530

    simple demo of infrared remote control

---
+++ DemoTsop_iotwebplanet.ino
@@ -0,0 +1,55 @@
+/*
+ * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
+
+ * Copyright 2009 Ken Shirriff
+ 
+ */
+
+#include <IRremote.h>
+
+int RECV_PIN = 3;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+}
+
+void loop() {
+
+long on=16236607;
+long off=16203967;
+
+
+  
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    Serial.println(results.value);
+    if(results.value==on)
+    {
+      digitalWrite(13,HIGH);
+      Serial.println("ON");
+    }
+
+    if(results.value==off)
+    {
+      digitalWrite(13,LOW);
+      Serial.println("OFF");
+    }
+    
+    irrecv.resume(); // Receive the next value
+  }
+  delay(100);
+}
+
+
+
+
